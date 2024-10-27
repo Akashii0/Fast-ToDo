b7a660f4c2773f4266ba7ed913ce575075fec783
Akashii0 <abdulkid151@gmail.com>
2024-10-27 21:46:30 +0100

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from typing import Union
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"Message":"Hello World."}
+# @app.get('/')
+# def read_root():
+#     return {"Message":"Hello World."}
 
-@app.get('items/{item_id}')
-def getItems(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get('items/{item_id}')
+# def getItems(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
