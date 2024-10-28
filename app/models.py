170e2d5a98d964086c2420b852d36c3686d32e93
Akashii0 <abdulkid151@gmail.com>
2024-10-28 20:53:49 +0100

    updated models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from sqlalchemy import Boolean, Column, Integer, String
-
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from database import Base
+
+class ToDo(Base):
+    __tablename__ = "ToDos"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    content = Column(String, nullable=False)
+    status = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
+    
