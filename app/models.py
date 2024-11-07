ef4ede5cbe6015a72ce68d187126561f5f8cf1ae
Akashii0 <abdulkid151@gmail.com>
2024-11-07 18:32:30 +0100

    updated models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from .database import Base
 
@@ -9,6 +10,17 @@ class ToDo(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     content = Column(String, index=True)
     status = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, nullable=False, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
                         server_default=text('now()'))
     
