9f56bd28d0880d66529db3d6eac76d294b01dbc0
Akashii0 <abdulkid151@gmail.com>
2024-11-07 20:04:00 +0100

    updated crud.py

--- app/routers/crud.py
+++ app/routers/crud.py
@@ -27,25 +27,40 @@ async def index(request: Request, db: Session = Depends(get_db), current_user: i
                                       {"request": request, "todo_list": todos})
 
 @router.post('/add')
-def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
-    new_todo = models.ToDo(content=title)
+def add(request: Request, title: str = Form(...), db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    new_todo = models.ToDo(owner_id= current_user.id, content=title)
     db.add(new_todo)
     db.commit()
+    db.refresh(new_todo)
 
     return RedirectResponse(url='/', status_code = status.HTTP_303_SEE_OTHER)
 
 @router.get('/update/{todo_id}')
-def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+def update(request: Request, todo_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     todo = db.query(models.ToDo).filter(models.ToDo.id == todo_id).first()
     todo.status = not todo.status
     db.commit()
     
+    if todo.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+    
     return RedirectResponse(url='/todo', status_code = status.HTTP_302_FOUND)
     
 @router.get("/delete/{todo_id}")
-def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.ToDo).filter(models.ToDo.id == todo_id).first()
-    db.delete(todo)
+def delete(request: Request, todo_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    todo_query = db.query(models.ToDo).filter(models.ToDo.id == todo_id)
+    todo = todo_query.first()
+    
+    if todo == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail= f"Post with the id: {id} does not exist.")
+    
+    if todo.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+        
+    todo_query.delete(synchronize_session=False)
     db.commit()
     
     return RedirectResponse(url='/todo', status_code=status.HTTP_302_FOUND)
