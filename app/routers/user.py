b47e24d209b3431aae893eeb299df9ca4833f22d
Akashii0 <abdulkid151@gmail.com>
2024-11-08 23:56:59 +0100

    Updated user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,26 @@
 from fastapi import status, HTTPException, Depends, APIRouter
-import models, schemas, utils
-from database import get_db
+from .. import models, schemas, utils
+from ..database import get_db
 from sqlalchemy.orm import Session
 
+router = APIRouter(
+    prefix="/users",
+    tags=['Users']
+)
 
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    # Hash the password - user.password
+    
+    # hashed_password = pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
