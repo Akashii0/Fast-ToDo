81a99c874e57efdb226a4a7920167956d79b561e
Akashii0 <abdulkid151@gmail.com>
2024-11-09 15:59:45 +0100

    Updated schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,35 +1,38 @@
-from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
-from typing import Optional
-from typing_extensions import Annotated
+# from typing import Optional
 
-from pydantic import BaseModel, Field
+from fastapi import Form
+from pydantic import BaseModel
+# from typing_extensions import Annotated
 
 
 class UserCreate(BaseModel):
     username: str
     password: str
-    
+
+
 class UserOut(BaseModel):
     id: int
     username: str
     created_at: datetime
-    
+
     class Config:
         orm_mode = True
-        
+
+
 class UserLogin(BaseModel):
-    username: str
-    password: str
+    username: str = Form()
+    password: str = Form()
 
 
 # Setting up a schema for the token
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
-    
+
+
 class TokenData(BaseModel):
     id: str | None = None
     # id: Optional[str] = None
-
