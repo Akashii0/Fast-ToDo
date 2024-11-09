72e7859f98acf3cb465f0b4577a1e5f87ba458fa
Akashii0 <abdulkid151@gmail.com>
2024-11-09 18:43:22 +0100

    Updated user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -11,9 +11,8 @@ from ..database import get_db
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(
-    user: schemas.UserCreate,
     db: Session = Depends(get_db),
     username: str = Form(),
     password: str = Form(),
@@ -22,38 +21,39 @@ def create_user(
 
     # hashed_password = pwd_context.hash(user.password)
     hashed_password = utils.hash(password)
-    password = hashed_password
 
-    new_user = models.User(**user.model_dump(username=username, password=password))
-    # new_user = User(username=username, password=password)
+    # new_user = User(**user.model_dump(username=username, password=password))
+    new_user = User(username=username, password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
+    print(new_user)
     return new_user
+    
 
 
-def get_user(self):
-    return self.sess.query(models.User).all()
+# def get_user(self):
+#     return self.sess.query(models.User).all()
 
 
-def get_user_by_username(self, username: str):
-    return self.sess.query(User).filter(User.username == username).first()
+# def get_user_by_username(self, username: str):
+#     return self.sess.query(User).filter(User.username == username).first()
 
 
-def update_user(self, id: int, details: Dict[str, Any]) -> bool:
-    try:
-        self.sess.query(User).filter(User.id == id).update(details)
-        self.sess.commit()
-    except:
-        return False
-    return True
+# def update_user(self, id: int, details: Dict[str, Any]) -> bool:
+#     try:
+#         self.sess.query(User).filter(User.id == id).update(details)
+#         self.sess.commit()
+#     except:
+#         return False
+#     return True
 
 
-def delete_user(self, id: int) -> bool:
-    try:
-        self.sess.query(User).filter(User.id == id).delete()
-        self.sess.commit()
-    except:
-        return False
-    return True
+# def delete_user(self, id: int) -> bool:
+#     try:
+#         self.sess.query(User).filter(User.id == id).delete()
+#         self.sess.commit()
+#     except:
+#         return False
+#     return True
