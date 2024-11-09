494503f8525ce16f007ee50780882da5cd7bcd62
Akashii0 <abdulkid151@gmail.com>
2024-11-09 15:59:55 +0100

    Updated auth.py routh

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,29 +1,54 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import (
+    APIRouter,
+    Depends,
+    Form,
+    HTTPException,
+    Request,
+    Response,
+    status,
+)
+from fastapi.responses import RedirectResponse
+# from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
-from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
-from .. import database, schemas, models, utils, oauth2
+from app import database, models, oauth2, schemas, utils
+from app.routers.crud import templates
 
 router = APIRouter(tags=["Authentication"])
+router.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
-@router.post('/login', response_model=schemas.Token)
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
-    
-    user = db.query(models.User).filter(
-        models.User.username == user_credentials.username).first()
 
+@router.get("/login")
+def showlogin(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.post("/login", response_model=schemas.Token)
+def login(
+    username: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(database.get_db),
+):
+    user = db.query(models.User).filter(models.User.username == username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
+        )
 
-    if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
-    
+    if not utils.verify(password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
+        )
+
     # Create a Token
     # Return token
-    
-    access_token = oauth2.create_access_token(data = {"user_id": user.id})
-    
-    # return {"token":"example token"}
-    return {"access_token" : access_token, "token_type" : "bearer"}
-    
-    
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    resp = RedirectResponse(url='/todo', status_code=303)
+
+    resp.set_cookie(key="token", value=access_token)
+
+    return resp
+
