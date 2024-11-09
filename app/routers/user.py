9f2baa099db26466d2ba611a99c6d493259f3043
Akashii0 <abdulkid151@gmail.com>
2024-11-09 18:45:52 +0100

    Updated user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
 from fastapi import APIRouter, Depends, Form, status
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from app.models import User
@@ -29,31 +30,30 @@ def create_user(
     db.refresh(new_user)
 
     print(new_user)
-    return new_user
-    
+    return RedirectResponse(url="/login", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 
-# def get_user(self):
-#     return self.sess.query(models.User).all()
+def get_user(self):
+    return self.sess.query(models.User).all()
 
 
-# def get_user_by_username(self, username: str):
-#     return self.sess.query(User).filter(User.username == username).first()
+def get_user_by_username(self, username: str):
+    return self.sess.query(User).filter(User.username == username).first()
 
 
-# def update_user(self, id: int, details: Dict[str, Any]) -> bool:
-#     try:
-#         self.sess.query(User).filter(User.id == id).update(details)
-#         self.sess.commit()
-#     except:
-#         return False
-#     return True
+def update_user(self, id: int, details: Dict[str, Any]) -> bool:
+    try:
+        self.sess.query(User).filter(User.id == id).update(details)
+        self.sess.commit()
+    except:
+        return False
+    return True
 
 
-# def delete_user(self, id: int) -> bool:
-#     try:
-#         self.sess.query(User).filter(User.id == id).delete()
-#         self.sess.commit()
-#     except:
-#         return False
-#     return True
+def delete_user(self, id: int) -> bool:
+    try:
+        self.sess.query(User).filter(User.id == id).delete()
+        self.sess.commit()
+    except:
+        return False
+    return True
